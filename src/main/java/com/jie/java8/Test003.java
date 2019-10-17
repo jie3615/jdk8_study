a31c38c5731058f918b29afdf3048cd30dd04b9b
ajie <11jkyj@sina.com>
2019-10-17 19:41:03 +0800

    lambda，方法引用

---
+++ src/main/java/com/jie/java8/Test003.java
@@ -0,0 +1,82 @@
+package com.jie.java8;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: wyj
+ * @date: 2019/10/17
+ * @description:
+ */
+public class Test003 {
+    /**
+     * 类型推断:根据参数类型和返回结果的类型推断
+     * @param args
+     */
+    public static void main(String[] args){
+
+        MyInterface01 myInterface01 = ()->{
+//            System.out.println("interface1");
+        };
+        MyInterface02 myInterface02 = ()->{
+//            System.out.println("interface2");
+        };
+
+        System.out.println(myInterface01.getClass().getInterfaces()[0]);
+        System.out.println(myInterface02.getClass().getInterfaces()[0]);
+    }
+
+    // 线程的定义
+    @Test
+    public void test01() {
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName()+"线程执行");
+        }).start();
+    }
+
+    /**
+     * java8小写转大写
+     */
+    @Test
+    public void test02() {
+       List<String> list = Arrays.asList("hello","world","hello wyj");
+       list.forEach(item-> System.out.println(item.toUpperCase()));
+
+        System.out.println("#########");
+       // 放入新的集合
+        List<String> list1 = new ArrayList<>();
+        list.forEach(item->{
+            String s = item.toUpperCase();
+            list1.add(s);
+        });
+        list1.forEach(System.out::println);
+
+        System.out.println("#########");
+        //使用stream
+        list.stream();//串行流
+        list.parallelStream();// 并行流
+        // 使用lambda
+        list.stream().map(item->item.toUpperCase()).forEach(System.out::println);
+        System.out.println("#########");
+        //使用方法引用，使用类或者对象都可以进行方法引用； toUpperCase()的参数是调用者
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+
+
+
+    }
+
+
+}
+
+    @FunctionalInterface
+     interface MyInterface01{
+        void test01();
+     }
+
+    @FunctionalInterface
+    interface MyInterface02{
+        void test02();
+    }
