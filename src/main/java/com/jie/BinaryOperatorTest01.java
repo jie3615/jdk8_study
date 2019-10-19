71225ea853697645ff8b3ad5517be44752ef415e
ajie <11jkyj@sina.com>
2019-10-19 17:28:18 +0800

    BinaryOperator的静态方法用法

--- src/main/java/com/jie/BinaryOperatorTest01.java
+++ src/main/java/com/jie/BinaryOperatorTest01.java
@@ -2,6 +2,7 @@ package com.jie;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.function.BinaryOperator;
 
 /**
@@ -19,6 +20,21 @@ public class BinaryOperatorTest01 {
     @Test
     public void test01() {
         System.out.println(calculate(1, 3, (x, y) -> x + y));
+    }
+
+    // BinaryOperator 的静态方法使用minBy
+
+    public String getIWantString(String x, String y, Comparator<String> comparator) {
+        return BinaryOperator.minBy(comparator).apply(x, y);
+    }
+
+
+    @Test
+    public void test02() {
+        // 选取长度短的字符串
+        System.out.println(getIWantString("jiege", "ajie", (x, y) -> x.length()-y.length()));
+
+        System.out.println(getIWantString("jiege", "ajie", Comparator.comparingInt(String::length)));
     }
 
 
